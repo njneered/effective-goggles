fd90255608fbf83dbaa675170479960c8a73f3a6
Meaghan Knowles <knowles.mc@ufl.edu>
2023-10-30 18:44:05 -0400

    This is the decoded function

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("hi this is meaghan")
+
+
+#decode function
+def decode_password(decode_input):
+	decode_input_list = []
+	for i in decode_input:
+		i = int(i) - 3
+		decode_input_list.append(i)
+		decoded_string = "".join(map(str, decode_input_list))
+	return decoded_string
+
 
